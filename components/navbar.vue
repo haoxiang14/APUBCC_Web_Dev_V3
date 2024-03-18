d432195a7df6070b7d59f2b9316affea26c45422
haoxiang-14 <haoxiang@coingecko.com>
2024-03-19 01:50:24 +0800

    Add external links icon

--- components/navbar.vue
+++ components/navbar.vue
@@ -14,7 +14,7 @@
                 </div>
                 <ul tabindex="0" class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
                     <li>
-                        <a class="font-bold text-xl hover:bg-primary hover:text-white">ABOUT US</a>
+                        <a class="font-bold text-xl">ABOUT US</a>
                         <ul class="p-2">
                             <li><NuxtLink to="/vision-mission" class="hover:bg-primary hover:text-white"> VISION & MISSION </NuxtLink></li>
                             <li><NuxtLink to="/partners" class="hover:bg-primary hover:text-white"> PARTNERS </NuxtLink></li>
@@ -23,11 +23,11 @@
                     </li>
                     <li><NuxtLink to="/events" class="font-bold text-xl hover:bg-primary hover:text-white">EVENTS</NuxtLink></li>
                     <li>
-                        <a class="font-bold text-xl hover:bg-primary hover:text-white">Resources</a>
+                        <a class="font-bold text-xl">Resources</a>
                         <ul class="p-2">
-                            <li><a href="https://apubcc.substack.com/" target="_blank" class="hover:bg-primary hover:text-white"> SUBSTACK </a></li>
-                            <li><a href="https://github.com/apubcc" target="_blank" class="hover:bg-primary hover:text-white"> GITHUB </a></li>
-                            <li><a href="https://onboard.stackup.dev/community/apubcc" target="_blank" class="hover:bg-primary hover:text-white"> ONBOARD </a></li>
+                            <li><a href="https://apubcc.substack.com/" target="_blank" class="hover:bg-primary hover:text-white"> SUBSTACK <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-external-link"><path d="M15 3h6v6"/><path d="M10 14 21 3"/><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"/></svg> </a></li>
+                            <li><a href="https://github.com/apubcc" target="_blank" class="hover:bg-primary hover:text-white">  GITHUB <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-external-link"><path d="M15 3h6v6"/><path d="M10 14 21 3"/><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"/></svg> </a></li>
+                            <li><a href="https://onboard.stackup.dev/community/apubcc" target="_blank" class="hover:bg-primary hover:text-white"> ONBOARD <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-external-link"><path d="M15 3h6v6"/><path d="M10 14 21 3"/><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"/></svg> </a></li>
                         </ul>
                     </li>
                     <li><NuxtLink to="/#faq" class="font-bold text-xl hover:bg-primary hover:text-white"> FAQ </NuxtLink> </li>
@@ -51,9 +51,9 @@
                     <details>
                         <summary> RESOURCES </summary>
                             <ul class="p-2 z-[1]">
-                                <li><a href="https://apubcc.substack.com/" target="_blank" class="text-base hover:bg-primary hover:text-white"> SUBSTACK </a></li>
-                                <li><a href="https://github.com/apubcc" target="_blank" class="text-base hover:bg-primary hover:text-white"> GITHUB </a></li>
-                                <li><a href="https://onboard.stackup.dev/community/apubcc" target="_blank" class="text-base hover:bg-primary hover:text-white"> ONBOARD </a></li>
+                                <li><a href="https://apubcc.substack.com/" target="_blank" class="text-base hover:bg-primary hover:text-white"> SUBSTACK <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-external-link"><path d="M15 3h6v6"/><path d="M10 14 21 3"/><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"/></svg> </a></li>
+                                <li><a href="https://github.com/apubcc" target="_blank" class="text-base hover:bg-primary hover:text-white"> GITHUB <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-external-link"><path d="M15 3h6v6"/><path d="M10 14 21 3"/><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"/></svg> </a></li>
+                                <li><a href="https://onboard.stackup.dev/community/apubcc" target="_blank" class="text-base hover:bg-primary hover:text-white"> ONBOARD <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-external-link"><path d="M15 3h6v6"/><path d="M10 14 21 3"/><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"/></svg> </a></li>
                             </ul>
                     </details>
                 </li>
